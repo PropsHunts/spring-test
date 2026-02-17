1f461456a5c68ddbb673c226a394c28b85d92ef6
PropsHunts <htppuriel@gmail.com>
2026-02-17 13:18:28 +0200

    17/2/26 13:17 Update for SpringTestApp

--- src/main/java/propst/spring_test/SpringTestApplication.java
+++ src/main/java/propst/spring_test/SpringTestApplication.java
@@ -8,6 +8,8 @@ public class SpringTestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringTestApplication.class, args);
+		System.out.println("==> app running...");
+		
 	}
 
 }
